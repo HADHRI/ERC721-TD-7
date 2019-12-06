d51f30827db066c85b521754165057290c00b3ab
HADHRI <hadhri.anas@gmail.com>
2019-12-06 21:34:00 +0100

    Getting chainId and lastblockNumber from blochChain

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
+import Web3 from 'web3';
 import './App.css';
+class App extends Component{
 
-function App() {
+  componentWillMount(){
+    this.loadBlockchainData()
+  }
+  
+  //Interacting with  the blockchain 
+  //retrieving the chainId and the lastBlockNumber
+  async loadBlockchainData(){
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    //using await because it is a sync function
+   const network= await web3.eth.net.getNetworkType()
+   const chainId =await web3.eth.getChainId()
+   const lastBlock= await web3.eth.getBlock('latest')
+   this.setState({chainId : chainId,lastBlockNumber : lastBlock.number,
+  network : network})
+  } 
+
+  constructor(props){
+    super(props)
+    this.state = {chainId :'',lastBlockNumber:''}
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>My React App </h1>
+      <p>   Your network is {this.state.network}</p>
+      <p>   Chain Id = {this.state.chainId}</p>
+      <p>   Last Block Number = {this.state.lastBlockNumber}</p> 
     </div>
   );
-}
+}}
 
 export default App;
